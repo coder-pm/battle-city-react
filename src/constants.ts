c5bdb839c988525c3d0f01f6ae272c02c4aff84b
Marcin Polak <marcin@coder.pm>
2019-05-06 13:34:19 +0200

    Environment configuration (server address for development)

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const GAME_SERVER_ADDRESS: string = '192.168.1.101';
-export const GAME_SERVER_PORT: number = 3100;
+export const GAME_SERVER_ADDRESS: string = process.env.NODE_ENV === 'development' ? '192.168.1.101' : 'battlecity-online.coder.pm';
+export const GAME_SERVER_PORT: number = process.env.NODE_ENV === 'development' ? 3100 : 443;
 export const GAME_FRAMERATE: number = Math.floor(1000 / 60);
 export const BOARD_WIDTH: number = 1080 + (2 * 24);
 export const BOARD_HEIGHT: number = 720 + (2 * 24);
